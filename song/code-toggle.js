459594c69551576796dacf3ff071b319a87d9c89
jtran-davinci <justin_tran@student.davincischools.org>
2026-03-17 11:39:23 -0700

    Update
    Updated file ordering and created structure to data

--- song/code-toggle.js
+++ song/code-toggle.js
@@ -1,8 +1,21 @@
 const showCodeBtn = document.getElementById('showCode');
 const codeOverlay = document.getElementById('codeOverlay');
+const codeBlock = document.getElementById("codeBlock");
 
 showCodeBtn.addEventListener('click', () => {
     codeOverlay.classList.toggle('active');
 
     showCodeBtn.textContent = codeOverlay.classList.contains('active') ? 'Hide Code' : 'Show Code';
 });
+
+fetch("/song-data/justint/code.rb")
+  .then(response => response.text())
+  .then(data => {
+    codeBlock.textContent = data;
+
+    Prism.highlightElement(codeBlock);
+  })
+  .catch(err => {
+    codeBlock.textContent = "Failed to load code.";
+    console.error(err);
+  });
