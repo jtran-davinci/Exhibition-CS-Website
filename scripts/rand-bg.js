4515f192e7cf8e903ab7f1683250d9687b9f9ebe
jtran-davinci <justin_tran@student.davincischools.org>
2026-03-18 17:08:45 -0700

    Urgent fixes

--- scripts/rand-bg.js
+++ scripts/rand-bg.js
@@ -1,8 +1,8 @@
 const backgrounds = [
-    "../images/background/default-bg.gif",
-    "../images/background/backParty2.gif",
-    "../images/background/backLights4.gif",
-    "../images/background/backgroundLights3.gif"
+    "images/background/default-bg.gif",
+    "images/background/backParty2.gif",
+    "images/background/backLights4.gif",
+    "images/background/backgroundLights3.gif"
 ];
 
 const randomIndex = Math.floor(Math.random() * backgrounds.length);
